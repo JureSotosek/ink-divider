23631b65d59532a58bd69911783b27d8f224f836
LitoMore <litomore@gmail.com>
2019-10-13 03:11:01 +0800

    Fix failing test

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
 import test from 'ava';
 import React from 'react';
+import {Box, Color} from 'ink';
 import {render} from 'ink-testing-library';
 import Divider from '.';
 
-test('Basic divider', t => {
-	const {lastFrame} = render(<Divider title="Title"/>);
+const PAD = ' ';
+const dividerChar = '─';
 
-	console.log(lastFrame());
-	t.snapshot(lastFrame());
+test('Basic divider', t => {
+	const {lastFrame: actual} = render(<Divider title="Title"/>);
+	const {lastFrame: expected} = render(
+		<Box flexDirection="row">
+			{PAD}
+			<Color keyword="grey">{dividerChar.repeat(21)}</Color>
+			<Color keyword="white">{`${PAD}Title${PAD}`}</Color>
+			<Color keyword="grey">{dividerChar.repeat(21)}</Color>
+			{PAD}
+		</Box>
+	);
+
+	t.is(actual(), expected());
 });
