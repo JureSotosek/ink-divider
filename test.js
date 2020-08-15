39235b4309c541fe8bdd166adef1c7de7a80bf65
omjadas <omja.das@gmail.com>
2020-08-15 10:06:40 +1000

    feat(ink)!: Migrate to ink v3

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import React from 'react';
-import {Box, Color} from 'ink';
+import {Box, Text} from 'ink';
 import {render} from 'ink-testing-library';
 import Divider from '.';
 
@@ -11,11 +11,13 @@ test('Basic divider', t => {
 	const {lastFrame: actual} = render(<Divider title="Title"/>);
 	const {lastFrame: expected} = render(
 		<Box flexDirection="row">
-			{PAD}
-			<Color keyword="grey">{dividerChar.repeat(21)}</Color>
-			<Color keyword="white">{`${PAD}Title${PAD}`}</Color>
-			<Color keyword="grey">{dividerChar.repeat(21)}</Color>
-			{PAD}
+			<Text>
+				{PAD}
+				<Text color="grey">{dividerChar.repeat(21)}</Text>
+				<Text color="white">{`${PAD}Title${PAD}`}</Text>
+				<Text color="grey">{dividerChar.repeat(21)}</Text>
+				{PAD}
+			</Text>
 		</Box>
 	);
 
