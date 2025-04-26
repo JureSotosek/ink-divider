93273a079fe5ce209add8506a9dd4b9323cb0484
Okinea Dev <hi@okinea.dev>
2025-04-26 14:57:23 +0200

    perf(build): ⚡ migrate from `tsup` to `bunup`
    It is incredibly fast
    
    https://bunup.dev

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'bunup'
 
 export default defineConfig({
 	entry: ['src/index.tsx'],
 	format: ['esm'],
 	dts: true, // Generate declaration file (.d.ts)
-	external: ['yoga-wasm-web', 'react'], // Don't include this packages in bundle
+	external: ['yoga-wasm-web', 'react'], // Exclude these packages from the bundle
 	minify: true,
-	sourcemap: true,
 	clean: true,
 })
